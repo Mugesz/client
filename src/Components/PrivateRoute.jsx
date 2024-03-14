9a67aac29e4e304dc56c93cbad3eacd9919dae80
frekerzzzz <kannamugesh135@gmail.com>
2024-03-14 17:36:52 +0530

    navigaye

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 
 const PrivateRoute = () => {
-    const navigate = useNavigate()
+  
   const isAuthenticated = localStorage.getItem('token');
 
   return (
    isAuthenticated ? (
     <Outlet/>
    ) :(
-    navigate("/")
+    
+    <Navigate to={"/"}/>
    )
   );
 };
